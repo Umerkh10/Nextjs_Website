4f6f091f3d7b8da3d687cf8e25c3436ee6a87ec1
umer <mumarkhan383@gmail.com>
2024-07-27 14:46:45 +0500

    digitalchange

--- src/app/(frontend)/Slider.jsx
+++ src/app/(frontend)/Slider.jsx
@@ -76,7 +76,7 @@ const Slider = () => {
                 <span className='inline-block text-white font-extrabold uppercase text-[80px] md:text-[140px] lg:-translate-y-24 lg:translate-x-[-3rem] md:-translate-y-[5.5rem]  -translate-y-12 translate-x-4 text-to-reveal'>Ive</span>
 
                 <span className='inline-block rotate-[90deg] font-black text-white text-[22px] md:text-[42px] 
-                translate-x-[6rem] translate-y-[-9.5rem] md:-translate-x-8 md:-translate-y-28 lg:-translate-y-[215px] lg:translate-x-[143px] xl:-translate-y-[115px] xl:translate-x-[-95px] text-to-reveal'>Digital</span>
+                translate-x-[7rem] translate-y-[-9.5rem] md:-translate-x-8 md:-translate-y-28 lg:-translate-y-[215px] lg:translate-x-[143px] xl:-translate-y-[115px] xl:translate-x-[-95px] text-to-reveal'>Digital</span>
               </div>
               <div className='h-auto w-full'>
                 <span className='inline-block -translate-y-[5rem] translate-x-4 lg:-translate-y-[14rem] lg:translate-x-[-3.5rem] xl:-translate-y-[165px] xl:translate-x-[-3.5rem] md:translate-x-4 md:-translate-y-[150px] font-extrabold text-white text-[30px] md:text-[60px] text-to-reveal'>Agency</span>
@@ -115,7 +115,7 @@ const Slider = () => {
                 <span className='inline-block text-white font-extrabold uppercase text-[80px] md:text-[140px] lg:-translate-y-24 lg:translate-x-[-3rem] md:-translate-y-[5.5rem]  -translate-y-12 translate-x-4 text-to-reveal'>Ive</span>
 
                 <span className='inline-block rotate-[90deg] font-black text-white text-[22px] md:text-[42px] 
-                translate-x-[6rem] translate-y-[-9.5rem] md:-translate-x-8 md:-translate-y-28 lg:-translate-y-[215px] lg:translate-x-[143px] xl:-translate-y-[115px] xl:translate-x-[-95px] text-to-reveal'>Digital</span>
+                translate-x-[7rem] translate-y-[-9.5rem] md:-translate-x-8 md:-translate-y-28 lg:-translate-y-[215px] lg:translate-x-[143px] xl:-translate-y-[115px] xl:translate-x-[-95px] text-to-reveal'>Digital</span>
               </div>
               <div className='h-auto w-full'>
                 <span className='inline-block -translate-y-[5rem] translate-x-4 lg:-translate-y-[14rem] lg:translate-x-[-3.5rem] xl:-translate-y-[165px] xl:translate-x-[-3.5rem] md:translate-x-4 md:-translate-y-[150px] font-extrabold text-white text-[30px] md:text-[60px] text-to-reveal'>Agency</span>
@@ -155,7 +155,7 @@ const Slider = () => {
                 <span className='inline-block text-white font-extrabold uppercase text-[80px] md:text-[140px] lg:-translate-y-24 lg:translate-x-[-3rem] md:-translate-y-[5.5rem]  -translate-y-12 translate-x-4 text-to-reveal'>Ive</span>
 
                 <span className='inline-block rotate-[90deg] font-black text-white text-[22px] md:text-[42px] 
-                translate-x-[6rem] translate-y-[-9.5rem] md:-translate-x-8 md:-translate-y-28 lg:-translate-y-[215px] lg:translate-x-[143px] xl:-translate-y-[115px] xl:translate-x-[-95px] text-to-reveal'>Digital</span>
+                translate-x-[7rem] translate-y-[-9.5rem] md:-translate-x-8 md:-translate-y-28 lg:-translate-y-[215px] lg:translate-x-[143px] xl:-translate-y-[115px] xl:translate-x-[-95px] text-to-reveal'>Digital</span>
               </div>
               <div className='h-auto w-full'>
                 <span className='inline-block -translate-y-[5rem] translate-x-4 lg:-translate-y-[14rem] lg:translate-x-[-3.5rem] xl:-translate-y-[165px] xl:translate-x-[-3.5rem] md:translate-x-4 md:-translate-y-[150px] font-extrabold text-white text-[30px] md:text-[60px] text-to-reveal'>Agency</span>
