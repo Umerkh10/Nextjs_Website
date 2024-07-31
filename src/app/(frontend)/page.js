ce06b84f315d643a21528f11e03be8b24a7ce2f1
umer <mumarkhan383@gmail.com>
2024-07-31 16:51:03 +0500

    lazyloadcomponets

--- src/app/(frontend)/page.js
+++ src/app/(frontend)/page.js
@@ -1,16 +1,16 @@
 
-import Navbar from './Navbar'
-import NavbarToggle from './NavbarToggle'
-import Slider from './Slider'
-import Rating from './Rating'
-import Image_Parallax from './Image_Parallax'
-import Card_Carousel from './Card_Carousel'
-import OurProject from './OurProject'
-import Work_Process from './Work_Process'
-import Testimonials from './Testimonials'
-import Footer from './Footer'
-import Revenue_Background from './Revenue_Background'
-import Loader from './Loader'
+import React, { Suspense } from 'react';
+const Navbar = React.lazy(() => import('./Navbar'));
+const Slider = React.lazy(() => import('./Slider'));
+const Rating = React.lazy(() => import('./Rating'));
+const Image_Parallax = React.lazy(() => import('./Image_Parallax'));
+const Card_Carousel = React.lazy(() => import('./Card_Carousel'));
+const OurProject = React.lazy(() => import('./OurProject'));
+const Work_Process = React.lazy(() => import('./Work_Process'));
+const Testimonials = React.lazy(() => import('./Testimonials'));
+const Footer = React.lazy(() => import('./Footer'));
+const Revenue_Background = React.lazy(() => import('./Revenue_Background'));
+
 
 export const metadata = {
   title: 'Home | Webnike',
@@ -21,20 +21,19 @@ export const metadata = {
 const HomePage = () => {
 
   return (
-    <div className='relative '>
-            <>
-              <Navbar />
-              <Slider />
-              <Rating />
-              <Image_Parallax />
-              <Card_Carousel />
-              <Revenue_Background />
-              <OurProject />
-              <Work_Process />
-              <Testimonials />
-              <Footer /></>
-      
-
+    <div className='relative'>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+        <Slider />
+        <Rating />
+        <Image_Parallax />
+        <Card_Carousel />
+        <Revenue_Background />
+        <OurProject />
+        <Work_Process />
+        <Testimonials />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
