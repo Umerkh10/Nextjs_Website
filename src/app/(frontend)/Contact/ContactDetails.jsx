202020df7ce4827c30050142db6868da964054fc
umer <mumarkhan383@gmail.com>
2024-08-03 15:28:33 +0500

    contactchanges

--- src/app/(frontend)/Contact/ContactDetails.jsx
+++ src/app/(frontend)/Contact/ContactDetails.jsx
@@ -23,29 +23,29 @@ const ContactDetails = () => {
   <div className='relative z-10'data-aos='fade-up' > 
     <div style={{backgroundImage:"url('/imgs/footer-bg.webp')",backgroundPosition:'center',backgroundSize:'cover',backgroundRepeat:'no-repeat' }}
     className=' md:max-w-[460px] m-auto xl:ml-[700px] pt-12 pb-12 pr-10 pl-10 h-[90vh] '>
-      <span className='text-gray-200 text-5xl font-extrabold flex justify-center'>Contact Info</span>
+      <span className='text-gray-200 text-3xl md:text-5xl font-extrabold flex justify-center'>Contact Info</span>
       <div className='flex  gap-0 mt-10'>
         <div className=' rounded-full h-16 w-24 bg-purple-700'><Image className='m-auto pt-3' src={"/imgs/contact_info_home.png"} width={35} height={40} alt='contact_info_home'  ></Image></div>
         <div className='flex flex-col'>
-          <div className='pl-4 text-gray-200 font-bold text-xl'>Office Address</div>
-          <div className='text-gray-200 opacity-70 pl-4'>60a Oldham St, Manchester M4 1LE, United Kingdom</div>
+          <div className='pl-4 text-gray-200 font-bold text-lg md:text-xl'>Office Address</div>
+          <div className='text-gray-200 opacity-70 text-sm md:text-base pl-4'>60a Oldham St, Manchester M4 1LE, United Kingdom</div>
           </div>
       </div>
 
       <div className='flex  gap-0 mt-10'>
         <div className=' rounded-full h-16 w-16 bg-purple-700'><Image className='m-auto pt-3' src={"/imgs/Contact_telephone.png"} width={35} height={40} alt='contact_info_home'  ></Image></div>
         <div className='flex flex-col'>
-          <div className='pl-3 text-gray-200 font-bold text-xl'>Phone Number</div>
-          <div className='text-gray-200 opacity-70 pl-3'>021-34155132</div>
-          <div className='text-gray-200 opacity-70 pl-3'>021-34155132</div>
+          <div className='pl-3 text-gray-200 font-bold text-lg md:text-xl'>Phone Number</div>
+          <div className='text-gray-200 opacity-70 text-sm md:text-base pl-3'>021-34155132</div>
+          <div className='text-gray-200 opacity-70 text-sm md:text-base pl-3'>021-34155132</div>
           </div>
       </div>
 
       <div className='flex  gap-0 mt-10'>
         <div className=' rounded-full h-16 w-16 bg-purple-700'><Image className='m-auto pt-3' src={"/imgs/email.png"} width={35} height={40} alt='contact_info_home'  ></Image></div>
         <div className='flex flex-col'>
-          <div className='pl-3 text-gray-200 font-bold text-xl'>Mail Address</div>
-          <div className='text-gray-200 opacity-70 pl-3'>support@webnike.com </div>
+          <div className='pl-3 text-gray-200 font-bold text-lg md:text-xl'>Mail Address</div>
+          <div className='text-gray-200 opacity-70 text-sm md:text-base pl-3'>support@webnike.com </div>
           </div>
       </div>
     </div>
--- src/app/(frontend)/Contact/ContactForm.jsx
+++ src/app/(frontend)/Contact/ContactForm.jsx
@@ -35,17 +35,17 @@ const ContactForm = () => {
     },[state])
     return (
         <div className='pt-28'>
-            <div className='grid lg:grid-cols-3 grid-cols-1  '>
+            <div className='grid lg:grid-cols-3 grid-cols-1 p-4 '>
                 <div className='grid-cols-1 col-span-1 ml-3' data-aos='fade-down'>
                     <Image src={"/imgs/contact-form.webp"} width={500} height={500} alt='contact-form'></Image>
                 </div>
                 <div className='grid-cols-1 col-span-2 ' data-aos='fade-up'>
                     <div className='lg:ml-40'>
-                        <div className='flex items-center text-purple-600 font-semibold text-lg gap-2'>
+                        <div className='flex items-center text-purple-600 font-semibold md:text-lg gap-2'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="" stroke="" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge stroke-purple-700 fill-purple-700"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" /></svg>
                             Contact Us</div>
-                        <div className=' text-6xl mt-2 font-black'>Get in Touch with Our</div>
-                        <div className='text-white text-stroke-3 text-6xl  mt-2 font-extrabold '>  Lovely Team</div>
+                        <div className=' text-4xl md:text-6xl mt-2 font-black'>Get in Touch with Our</div>
+                        <div className='text-white text-stroke-3 text-4xl md:text-6xl font-semibold mt-2 md:font-bold '>  Lovely Team</div>
                         
                         <form action={action}ref={formRef} >
                             <div className='grid lg:grid-cols-2 grid-cols-1 mt-5 gap-5'>
@@ -58,7 +58,7 @@ const ContactForm = () => {
                                     <div className='rounded-full pt-4 pb-4 mt-3  pl-4 pr-2 bg-slate-200 text-black'><input className='bg-slate-200 outline-none' type="text" name="subject" id="" placeholder='Enter Subject' required /></div>
                                 </div>
                             </div>
-                            <div className='rounded-xl h-[25vh] pt-4 pl-4 mt-4 bg-slate-200 text-black'><textarea name="cover" className=' bg-slate-200 outline-none' placeholder='Name Cover' id=""></textarea></div>
+                            <div className='rounded-xl h-[25vh]   pt-4 pl-4 mt-4 bg-slate-200 text-black'><textarea name="cover" className='p-2 bg-slate-200 w-full outline-none' placeholder='Name Cover' id=""></textarea></div>
                             <DynamicButton/>
                         </form>
                     </div>
@@ -77,7 +77,7 @@ function DynamicButton() {
         // <button disabled={pending} type='submit' className='flex items-center  font-semibold text-lg '>Get Consultations <ArrowRight className='ml-1  group-hover:translate-x-60 transition ease-out duration-150 delay-100' /></button>
         // </div>
 
-        <div className={`rounded-full w-1/4 mt-5 pt-4 pb-4 bg-violet-800 flex justify-center items-center group  hover:bg-violet-600 transition ease-in duration-200 delay-200 ${pending ? "text-opacity-70 bg-opacity-70 " : ""}`}>
+        <div className={`rounded-full md:w-[30%] mt-5 pt-4 pb-4 bg-violet-800 flex justify-center items-center group  hover:bg-violet-600 transition ease-in duration-200 delay-200 ${pending ? "text-opacity-70 bg-opacity-70 " : ""}`}>
             <button disabled={pending} className={` text-white font-semibold ${pending && 'animate-bounce'} `} type='submit'>Explore More</button>
         </div>
 
