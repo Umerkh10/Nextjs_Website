c8207d2e52c926bca5524a4e41b9ce4ae81b8a56
umer <mumarkhan383@gmail.com>
2024-08-05 13:25:04 +0500

    reverting

--- src/app/(frontend)/page.js
+++ src/app/(frontend)/page.js
@@ -1,15 +1,15 @@
 import dynamic from 'next/dynamic';
-const Navbar = dynamic(() => import('./Navbar'), { ssr: false });
-const Slider = dynamic(() => import('./Slider'), { ssr: false });
-const Rating = dynamic(() => import('./Rating'), { ssr: false });
-const ImageParallax = dynamic(() => import('./Image_Parallax'), { ssr: false });
-const CardCarousel = dynamic(() => import('./Card_Carousel'), { ssr: false });
-const OurProject = dynamic(() => import('./OurProject'), { ssr: false });
-const WorkProcess = dynamic(() => import('./Work_Process'), { ssr: false });
-const Testimonials = dynamic(() => import('./Testimonials'), { ssr: false });
-const Footer = dynamic(() => import('./Footer'), { ssr: false });
-const RevenueBackground = dynamic(() => import('./Revenue_Background'), { ssr: false });
 
+const Navbar = dynamic(() => import('./Navbar')  );
+const Slider = dynamic(() => import('./Slider'));
+const Rating = dynamic(() => import('./Rating'));
+const ImageParallax = dynamic(() => import('./Image_Parallax'));
+const CardCarousel = dynamic(() => import('./Card_Carousel'));
+const OurProject = dynamic(() => import('./OurProject'));
+const WorkProcess = dynamic(() => import('./Work_Process'));
+const Testimonials = dynamic(() => import('./Testimonials'));
+const Footer = dynamic(() => import('./Footer'));
+const RevenueBackground = dynamic(() => import('./Revenue_Background'));
 
 export const metadata = {
   title: 'Home | Webnike',
