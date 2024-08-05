d5976fbadf24cfe983ff17b45080ca8dbc2989e8
umer <mumarkhan383@gmail.com>
2024-08-05 14:54:59 +0500

    webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, options) {
+    // Check if BUNDLE_ANALYZE is set
+    if (process.env.BUNDLE_ANALYZE) {
+      // Add BundleAnalyzerPlugin
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: `./bundles/${process.env.BUNDLE_ANALYZE}.html`,
+        openAnalyzer: false,
+      }));
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
