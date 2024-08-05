ff5a465525e478d7d42c5e773dd63a6e643cf34e
umer <mumarkhan383@gmail.com>
2024-08-05 11:33:15 +0500

    dynamic

--- src/app/(frontend)/page.js
+++ src/app/(frontend)/page.js
@@ -1,40 +1,38 @@
+import dynamic from 'next/dynamic';
 
-import Navbar from './Navbar'
-import Slider from './Slider'
-import Rating from './Rating'
-import Image_Parallax from './Image_Parallax'
-import Card_Carousel from './Card_Carousel'
-import OurProject from './OurProject'
-import Work_Process from './Work_Process'
-import Testimonials from './Testimonials'
-import Footer from './Footer'
-import Revenue_Background from './Revenue_Background'
+const Navbar = dynamic(() => import('./Navbar'));
+const Slider = dynamic(() => import('./Slider'));
+const Rating = dynamic(() => import('./Rating'));
+const ImageParallax = dynamic(() => import('./Image_Parallax'));
+const CardCarousel = dynamic(() => import('./Card_Carousel'));
+const OurProject = dynamic(() => import('./OurProject'));
+const WorkProcess = dynamic(() => import('./Work_Process'));
+const Testimonials = dynamic(() => import('./Testimonials'));
+const Footer = dynamic(() => import('./Footer'));
+const RevenueBackground = dynamic(() => import('./Revenue_Background'));
 
 export const metadata = {
   title: 'Home | Webnike',
   description: "Webnike Portfolio Website",
 };
 
-
 const HomePage = () => {
-
   return (
-    <div className='relative '>
-            <>
-              <Navbar />
-              <Slider />
-              <Rating />
-              <Image_Parallax />
-              <Card_Carousel />
-              <Revenue_Background />
-              <OurProject />
-              <Work_Process />
-              <Testimonials />
-              <Footer /></>
-      
-
+    <div className='relative'>
+      <>
+        <Navbar />
+        <Slider />
+        <Rating />
+        <ImageParallax />
+        <CardCarousel />
+        <RevenueBackground />
+        <OurProject />
+        <WorkProcess />
+        <Testimonials />
+        <Footer />
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
