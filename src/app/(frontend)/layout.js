d6f9170558d2198a9944f7161dc95187f5e95e8e
umer <mumarkhan383@gmail.com>
2024-08-26 12:20:11 +0500

    reverting

--- src/app/(frontend)/layout.js
+++ src/app/(frontend)/layout.js
@@ -18,17 +18,8 @@ export const metadata = {
   },
   description: "Webnike Portfolio Website",
   robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: false,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
+  index: false,
+  follow: false,
   },
 
 };
