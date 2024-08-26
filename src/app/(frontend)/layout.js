f62ba83a88afad88a7aee8de91f3a7f094c30ee9
umer <mumarkhan383@gmail.com>
2024-08-26 12:02:33 +0500

    metarevert

--- src/app/(frontend)/layout.js
+++ src/app/(frontend)/layout.js
@@ -19,18 +19,8 @@ export const metadata = {
   description: "Webnike Portfolio Website",
   robots: {
     index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
+    follow: false,
   },
-
 
 };
 
